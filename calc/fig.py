556d72fdfaa08441fe611571e0a070268e5b89bb
sys010611 <a97968765@gmail.com>
2020-06-25 23:27:14 +0900

    issue #4
    add WSGI files and starting version control

---
+++ calc/fig.py
@@ -0,0 +1,23 @@
+from cgi import parse_qs
+from template import html
+
+def application(environ, start_response):
+	d = parse_qs(environ['QUERY_STRING'])
+	a = d.get('a','0')[0]
+	b = d.get('b','0')[0]
+
+	a, b = int(a), int(b)
+
+	response_body = html % {
+		'sum':[a+b],
+		'mul':[a*b],
+	}
+
+	status = '200 OK'
+	response_headers = [
+		('Content-Type', 'text/html'),
+		('Content-Length', str(len(response_body)))
+	]
+	
+	start_response(status, response_headers)
+	return [response_body]
---
+++ calc/template.py
@@ -0,0 +1,14 @@
+html = b"""
+<html>
+	<body>
+		<form method="get" action="">
+			a = <input type="number" name="a">
+			b = <input type="number" name="b">  
+			<input type="submit">
+		</form>
+			sum: %(sum)s
+			<p>
+			mul: %(mul)s
+	</body>
+</html>
+"""	
